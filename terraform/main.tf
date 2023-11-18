f9696419ce972b6c65b23c4c69de707fc23f8dd6
iampsrv <iampsrv@gmail.com>
2023-11-18 22:10:08 +0530

    terraform resources

---
+++ terraform/main.tf
@@ -0,0 +1,15 @@
+provider "aws" {
+  region = "us-east-1"
+}
+
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "~> 3.0.2"
+    }
+  }
+
+  required_version = ">= 1.1.0"
+}
+
